945c47a64025a366e71eade65a83a302794f9767
Ufuk Avcu <info@joomla-agentur.de>
2014-12-09 15:49:00 +0100

    Initial Alpha release

---
+++ jooag_shariff.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * @package 	JooAg Countdown
+ * @version 	3.x.0 Alpha
+ * @for 	Joomla 3.3+ 
+ * @author 	Joomla Agentur - http://www.joomla-agentur.de
+ * @copyright 	Copyright (c) 2009 - 2015 Joomla-Agentur All rights reserved.
+ * @license 	GNU General Public License version 2 or later;
+ * @description A small Plugin to Calculate the Days for a specific Date
+ */
+defined('_JEXEC') or die;
+
+class PlgContentJooag_shariff extends JPlugin
+{	
+
+    public function __construct(&$subject, $config)
+    {
+        $view = JFactory::getApplication()->input->getWord('view');
+
+        if($view != 'article'){
+            return;
+        }
+
+        parent::__construct($subject, $config);
+        $this->loadLanguage();
+    }
+	
+	public function onContentBeforeDisplay($context, &$article, &$params, $page = 0){
+		if($this->params->get('position') == 0){
+			$doc = JFactory::getDocument();
+			$lang = JFactory::getLanguage();
+			JHtml::_('jquery.framework');
+			$doc->addScript(JURI::root().'plugins/content/jooag_shariff/shariff.min.js');
+			$doc->addStyleSheet(JURI::root().'plugins/content/jooag_shariff/shariff.min.css');
+			
+			$services = implode("&quot;,&quot;", $this->params->get('services'));
+			$services = '&quot;'.$services.'&quot;';
+						
+			$output = '<div data-theme="'.$this->params->get('theme').'" data-lang="'.$lang->getLocale()[7].'"  data-orientation="'.$this->params->get('orientation').'" data-url="'.JURI::current().'" data-referrer-track="null" data-info-url="'.$this->params->get('info').'" data-services="['.$services.']" data-backend-url="/plugins/content/jooag_shariff/backend" class="shariff"></div>';
+			$output .= '<script src="plugins/content/jooag_shariff/shariff.min.js"></script>';
+			
+			return $output;
+		}
+	}
+	
+	public function onContentAfterDisplay($context, &$article, &$params, $page = 0){
+		if($this->params->get('position') == 1){
+			$doc = JFactory::getDocument();
+			$lang = JFactory::getLanguage();
+			JHtml::_('jquery.framework');
+			$doc->addScript(JURI::root().'plugins/content/jooag_shariff/shariff.min.js');
+			$doc->addStyleSheet(JURI::root().'plugins/content/jooag_shariff/shariff.min.css');
+			
+			$services = implode("&quot;,&quot;", $this->params->get('services'));
+			$services = '&quot;'.$services.'&quot;';
+						
+			$output = '<div data-theme="'.$this->params->get('theme').'" data-lang="'.$lang->getLocale()[7].'"  data-orientation="'.$this->params->get('orientation').'" data-url="'.JURI::current().'" data-referrer-track="null" data-info-url="'.$this->params->get('info').'" data-services="['.$services.']" data-backend-url="/plugins/content/jooag_shariff/backend" class="shariff"></div>';
+			$output .= '<script src="plugins/content/jooag_shariff/shariff.min.js"></script>';
+			
+			return $output;
+		}
+	}
+}
