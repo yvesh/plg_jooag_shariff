71052755b10bf042eaec0c80a9af0bec624d4deb
David Jardin <d.jardin@djumla.de>
2014-12-11 09:38:02 +0100

    Code cleanup

--- jooag_shariff.php
+++ jooag_shariff.php
@@ -1,50 +1,88 @@
 <?php
 /**
- * @package 	JooAg Shariff
- * @version 	3.x.0 Alpha
- * @for 	Joomla 3.3+ 
- * @author 	Joomla Agentur - http://www.joomla-agentur.de
- * @copyright 	Copyright (c) 2009 - 2015 Joomla-Agentur All rights reserved.
- * @license 	GNU General Public License version 2 or later;
+ * @package    JooAg_Shariff
+ *
+ * @author     Joomla Agentur <info@joomla-agentur.de>
+ * @copyright  Copyright (c) 2009 - 2015 Joomla-Agentur All rights reserved.
+ * @license    GNU General Public License version 2 or later;
  * @description A small Plugin to share Social Links!
  */
 defined('_JEXEC') or die;
 
-class PlgContentJooag_shariff extends JPlugin
-{	
-	public function __construct(&$subject, $config){
-		$view = JFactory::getApplication()->input->getWord('view');
-		if($view != 'article'){
-			return;
-		}
-		parent::__construct($subject, $config);
-	}
-	
-	public function onContentBeforeDisplay($context, &$article, &$params, $page = 0){
+/**
+ * Class PlgContentJooag_Shariff
+ *
+ * @since  1.0.0
+ */
+class PlgContentJooag_Shariff extends JPlugin
+{
+	/**
+	 * renders the buttons before the article
+	 *
+	 * @param   string   $context   The context of the content being passed to the plugin.
+	 * @param   mixed    &$article  An object with a "text" property
+	 * @param   mixed    &$params   Additional parameters. See {@see PlgContentContent()}.
+	 * @param   integer  $page      Optional page number. Unused. Defaults to zero.
+	 *
+	 * @return  string
+	 */
+	public function onContentBeforeDisplay($context, &$article, &$params, $page = 0)
+	{
 		$output = $this->getOutput('0');
+
 		return $output;
 	}
-	
-	public function onContentAfterDisplay($context, &$article, &$params, $page = 0){
+
+	/**
+	 * renders the buttons after the article
+	 *
+	 * @param   string   $context   The context of the content being passed to the plugin.
+	 * @param   mixed    &$article  An object with a "text" property
+	 * @param   mixed    &$params   Additional parameters. See {@see PlgContentContent()}.
+	 * @param   integer  $page      Optional page number. Unused. Defaults to zero.
+	 *
+	 * @return  string
+	 */
+	public function onContentAfterDisplay($context, &$article, &$params, $page = 0)
+	{
 		$output = $this->getOutput('1');
+
 		return $output;
 	}
-	
-	public function getOutput($position){
+
+	/**
+	 * appends the required scripts to the documents and returns the markup
+	 *
+	 * @param   integer  $position  current position
+	 *
+	 * @return string
+	 */
+	public function getOutput($position)
+	{
 		$setCatId = $this->params->get('showbycategory');
 		$currentCatId = JFactory::getApplication()->input->getInt('catid');
 		$output = '';
-		if($this->params->get('position') == $position and ((is_array($setCatId) && in_array($currentCatId,$setCatId)) or empty($setCatId))){
+
+		if ($this->params->get('position') == $position AND ((is_array($setCatId) && in_array($currentCatId, $setCatId)) OR empty($setCatId)))
+		{
 			$doc = JFactory::getDocument();
 			$lang = JFactory::getLanguage();
 			JHtml::_('jquery.framework');
-			$doc->addScript(JURI::root().'plugins/content/jooag_shariff/shariff.min.js');
-			$doc->addStyleSheet(JURI::root().'plugins/content/jooag_shariff/shariff.min.css');
+
+			$doc->addScript(JURI::root() . 'plugins/content/jooag_shariff/shariff.min.js');
+			$doc->addStyleSheet(JURI::root() . 'plugins/content/jooag_shariff/shariff.min.css');
+
 			$services = implode("&quot;,&quot;", $this->params->get('services'));
-			$services = '&quot;'.$services.'&quot;';
-			$output = '<div data-theme="'.$this->params->get('theme').'" data-lang="'.$lang->getLocale()[7].'"  data-orientation="'.$this->params->get('orientation').'" data-url="'.JURI::current().'" data-referrer-track="null" data-info-url="'.$this->params->get('info').'" data-services="['.$services.']" data-backend-url="/plugins/content/jooag_shariff/backend" class="shariff"></div>';
-			$output .= '<script src="plugins/content/jooag_shariff/shariff.min.js"></script>';
+			$services = '&quot;' . $services . '&quot;';
+
+			$output = '<div data-theme="' . $this->params->get('theme')
+				. '" data-lang="' . $lang->getLocale()[7]
+				. '" data-orientation="' . $this->params->get('orientation')
+				. '" data-url="' . JURI::current()
+				. '" data-info-url="' . $this->params->get('info')
+				. '" data-services="[' . $services . ']" data-backend-url="/plugins/content/jooag_shariff/backend" data-referrer-track="null" class="shariff"></div>';
 		}
+
 		return $output;
 	}
 }
