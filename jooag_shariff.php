9830fd32fbef8d4a6750472ec10d86d0934dc703
Ufuk Avcu <info@joomla-agentur.de>
2015-03-13 11:29:35 +0100

    Better Default Options

--- jooag_shariff.php
+++ jooag_shariff.php
@@ -100,6 +100,7 @@ class plgSystemJooag_Shariff extends JPlugin
 		JHtml::_('jquery.framework');
 				
 		$services = $this->params->get('services');
+		print_r (json_encode($services));
 		
 		if($this->params->get('info'))
 		{
